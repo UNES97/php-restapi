78b285a19fdda504e7f428a2caa374685c803ccd
Younes Noufair <48910083+UNES97@users.noreply.github.com>
2024-03-17 13:38:41 +0000

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,12 +1,17 @@
 <?php
 define("ROOT", $_SERVER["DOCUMENT_ROOT"] . "/api");
-include_once ROOT . "/functions/general.php";
+$routes = ["users"];
+$functions = ["general"];
+
+foreach ($functions as $f) {
+    include_once ROOT . "/functions/{$f}.php";
+}
+
 $envFilePath = ROOT . "/.env";
 $envData = parseEnvFile($envFilePath);
 include_once ROOT . "/configs/database.php";
 
 $requestUri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-
 $uriSegments = explode("/", rtrim($requestUri, "/"));
 $key = array_search("api", $uriSegments);
 if ($key !== false) {
@@ -17,8 +22,6 @@ if ($key !== false) {
 $t = count($uriSegments);
 $routeParent = $uriSegments[$t - ($t - 1)];
 $endpoint = end($uriSegments);
-
-$routes = ["users"];
 
 if (in_array($routeParent, $routes)) {
     include_once ROOT . "/routes/{$routeParent}.route.php";
